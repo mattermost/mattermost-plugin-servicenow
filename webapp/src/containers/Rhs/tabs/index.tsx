d931793072ace746e3bf9bcd1a5d64a09488013f
Sooraj Shukla <sooraj.shukla@brightscout.com>
2022-07-12 12:31:21 +0530

    [MI-1928]: AutoSuggest component

--- webapp/src/containers/Rhs/tabs/index.tsx
+++ webapp/src/containers/Rhs/tabs/index.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Tabs from 'components/tabs';
+import AutoSuggest from 'components/AutoSuggest';
 
 import './tabs.scss';
 
+const suggestions = [
+    'Suggestion1',
+    'Suggestion2',
+    'Suggestion3',
+    'Suggestion4',
+    'Suggestion5',
+];
+
 const Rhs = (): JSX.Element => {
+    const [inputValue, setInputValue] = useState('');
     const tabData: TabData[] = [
         {
             title: 'Current Channel Subscriptions',
-            tabPanel: <div>{'Tab Panel 1'}</div>,
+            tabPanel: <>
+                <AutoSuggest
+                    placeholder='Please enter some value'
+                    inputValue={inputValue}
+                    onInputValueChange={(newValue) => setInputValue(newValue)}
+                    suggestions={suggestions}
+                    charThresholdToShowSuggestions={4}
+                    loadingSuggestions={true}
+                />
+                <AutoSuggest
+                    placeholder='Please enter some value'
+                    inputValue={inputValue}
+                    onInputValueChange={(newValue) => setInputValue(newValue)}
+                    suggestions={[]}
+                    charThresholdToShowSuggestions={4}
+                />
+                <AutoSuggest
+                    placeholder='Please enter some value'
+                    inputValue={inputValue}
+                    onInputValueChange={(newValue) => setInputValue(newValue)}
+                    suggestions={suggestions}
+                    charThresholdToShowSuggestions={4}
+                    disabled={true}
+                />
+            </>,
         },
         {
             title: 'All Subscriptions',
