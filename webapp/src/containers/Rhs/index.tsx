ab9b660f9fda3766e4e004da6aaa4210f6f768d5
Sooraj Shukla <sooraj.shukla@brightscout.com>
2022-08-24 20:01:04 +0530

    [MI-2041]: review fixes

--- webapp/src/containers/Rhs/index.tsx
+++ webapp/src/containers/Rhs/index.tsx
@@ -48,7 +48,7 @@ const Rhs = (): JSX.Element => {
     const {currentChannelId} = useSelector((state: GlobalState) => state.entities.channels);
     const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
     const [toBeDeleted, setToBeDeleted] = useState<null | string>(null);
-    const [invalidDeleteApi, setInvalidDeleteApi] = useState(true);
+    const [deleteApiResponseInvalid, setDeleteApiResponseInvalid] = useState(true);
 
     const getSubscriptionsState = () => {
         const {isLoading, isSuccess, isError, data, error: apiErr} = getApiState(Constants.pluginApiServiceConfigs.fetchSubscriptions.apiServiceName, fetchSubscriptionParams as FetchSubscriptionsParams);
@@ -87,18 +87,18 @@ const Rhs = (): JSX.Element => {
     }, [refetchSubscriptions, showAllSubscriptions]);
 
     useEffect(() => {
-        if (getDeleteSubscriptionState().isSuccess && !invalidDeleteApi) {
+        if (getDeleteSubscriptionState().isSuccess && !deleteApiResponseInvalid) {
             setDeleteConfirmationOpen(false);
             dispatch(refetch());
-            setInvalidDeleteApi(true);
+            setDeleteApiResponseInvalid(true);
             setToBeDeleted(null);
         }
 
         // When a new API request is made, reset the flag set for invalid delete api response
         if (getDeleteSubscriptionState().isLoading) {
-            setInvalidDeleteApi(false);
+            setDeleteApiResponseInvalid(false);
         }
-    }, [getDeleteSubscriptionState().isSuccess, getDeleteSubscriptionState().isLoading, invalidDeleteApi]);
+    }, [getDeleteSubscriptionState().isSuccess, getDeleteSubscriptionState().isLoading, deleteApiResponseInvalid]);
 
     // Handles action when edit button is clicked for a subscription
     const handleEditSubscription = (subscription: SubscriptionData) => {
@@ -127,7 +127,7 @@ const Rhs = (): JSX.Element => {
     // Handles action when the delete confirmation modal is closed
     const hideDeleteConfirmation = () => {
         setDeleteConfirmationOpen(false);
-        setInvalidDeleteApi(true);
+        setDeleteApiResponseInvalid(true);
         setToBeDeleted(null);
     };
 
@@ -195,10 +195,10 @@ const Rhs = (): JSX.Element => {
                     confirmBtnText='Delete'
                     className='delete-confirmation-modal'
                     onConfirm={handleDeleteConfirmation}
-                    cancelDisabled={!invalidDeleteApi && getDeleteSubscriptionState().isLoading}
-                    confirmDisabled={!invalidDeleteApi && getDeleteSubscriptionState().isLoading}
-                    loading={!invalidDeleteApi && getDeleteSubscriptionState().isLoading}
-                    error={invalidDeleteApi || getDeleteSubscriptionState().isLoading || !getDeleteSubscriptionState().isError ? '' : getDeleteSubscriptionState().error}
+                    cancelDisabled={!deleteApiResponseInvalid && getDeleteSubscriptionState().isLoading}
+                    confirmDisabled={!deleteApiResponseInvalid && getDeleteSubscriptionState().isLoading}
+                    loading={!deleteApiResponseInvalid && getDeleteSubscriptionState().isLoading}
+                    error={deleteApiResponseInvalid || getDeleteSubscriptionState().isLoading || !getDeleteSubscriptionState().isError ? '' : getDeleteSubscriptionState().error}
                     confirmBtnClassName='btn-danger'
                 >
                     <>
