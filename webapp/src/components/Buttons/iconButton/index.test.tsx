9e0824c896a26d06327fbe3084c7085be2c5f20a
Manoj <manoj@brightscout.com>
2022-11-01 23:32:07 +0530

    [MI-2318] Added unit tests for some components Added unit tests for IconButton, Spinner and RHSHeader

---
+++ webapp/src/components/Buttons/iconButton/index.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import IconButton from 'components/Buttons/iconButton';
+
+describe('IconButton', () => {
+    const baseProps = {
+        tooltipText: 'mockTooltip',
+    };
+
+    it('should match snapshot with only tooltip text provided', () => {
+        const wrapper = shallow(<IconButton {...baseProps}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should match snapshot with all the props', () => {
+        const props = {
+            ...baseProps,
+            iconClassName: 'mockIconClassName',
+            extraClass: 'mockExtraClass',
+            onClick: jest.fn(),
+            children: (<span>{'Dummytext'}</span>),
+        };
+        const wrapper = shallow(<IconButton {...props}/>);
+        expect(wrapper).toMatchSnapshot();
+
+        expect(wrapper.find('Button').hasClass(props.extraClass)).toBeTruthy();
+        expect(wrapper.find('i').hasClass(props.iconClassName)).toBeTruthy();
+        wrapper.find('Button').simulate('click');
+        expect(props.onClick).toHaveBeenCalled();
+    });
+});
