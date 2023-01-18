f910abeceaf53c322fc23bdd68fb2af55d0ead5e
ayusht2810 <ayush.thakur@brightscout.com>
2023-01-18 11:46:29 +0530

    [MI-2638] Update slash commands

--- server/constants/constants.go
+++ server/constants/constants.go
@@ -116,19 +116,20 @@ const (
 	ContextNameRecordID   = "record_id"
 
 	// Slash commands
-	CommandHelp           = "help"
-	CommandConnect        = "connect"
-	CommandDisconnect     = "disconnect"
-	CommandSubscriptions  = "subscriptions"
-	CommandUnsubscribe    = "unsubscribe"
-	CommandSearchAndShare = "share"
-	CommandCreate         = "create"
-	SubCommandIncident    = "incident"
-	SubCommandRequest     = "request"
-	SubCommandList        = "list"
-	SubCommandAdd         = "add"
-	SubCommandEdit        = "edit"
-	SubCommandDelete      = "delete"
+	CommandHelp              = "help"
+	CommandConnect           = "connect"
+	CommandDisconnect        = "disconnect"
+	CommandSubscriptions     = "subscriptions"
+	CommandUnsubscribe       = "unsubscribe"
+	CommandRecords           = "records"
+	CommandCreate            = "create"
+	SubCommandIncident       = "incident"
+	SubCommandRequest        = "request"
+	SubCommandList           = "list"
+	SubCommandAdd            = "add"
+	SubCommandEdit           = "edit"
+	SubCommandDelete         = "delete"
+	SubCommandSearchAndShare = "share"
 )
 
 // #nosec G101 -- This is a false positive. The below line is not a hardcoded credential
