ce6c9ef44b727c3374d3b9e6afea7274e0d687c0
ayusht2810 <ayush.thakur@brightscout.com>
2024-06-04 15:11:29 +0530

    Update the logic to work synchronously and update log statements

--- server/plugin/kv.go
+++ server/plugin/kv.go
@@ -84,42 +84,37 @@ func (s *pluginStore) DeleteUser(mattermostUserID string) error {
 func (s *pluginStore) GetAllUsers() ([]*serializer.IncidentCaller, error) {
 	page := 0
 	users := []*serializer.IncidentCaller{}
-	wg := new(sync.WaitGroup)
 	mu := new(sync.Mutex)
 	for {
-		wg.Add(1)
 		kvList, err := s.plugin.API.KVList(page, constants.DefaultPerPage)
 		if err != nil {
 			return nil, err
 		}
 
-		go func() {
-			defer wg.Done()
-			for _, key := range kvList {
-				if userID, isValidUserKey := IsValidUserKey(key); isValidUserKey {
-					decodedKey, decodeErr := decodeKey(userID)
-					if decodeErr != nil {
-						s.plugin.API.LogError("Unable to decode key", "Key", key, "Error", decodeErr.Error())
-						return
-					}
-
-					user, loadErr := s.LoadUser(decodedKey)
-					if loadErr != nil {
-						s.plugin.API.LogError("Unable to load user", "UserID", decodedKey, "Error", loadErr.Error())
-						return
-					}
-
-					// Append the loaded user to the users slice under a lock.
-					mu.Lock()
-					users = append(users, &serializer.IncidentCaller{
-						MattermostUserID: user.MattermostUserID,
-						Username:         user.Username,
-						ServiceNowUser:   user.ServiceNowUser,
-					})
-					mu.Unlock()
+		for _, key := range kvList {
+			if userID, isValidUserKey := IsValidUserKey(key); isValidUserKey {
+				decodedKey, decodeErr := decodeKey(userID)
+				if decodeErr != nil {
+					s.plugin.API.LogError("Unable to decode key", "Key", key, "Error", decodeErr.Error())
+					break
 				}
+
+				user, loadErr := s.LoadUser(decodedKey)
+				if loadErr != nil {
+					s.plugin.API.LogError("Unable to load user", "UserID", decodedKey, "Error", loadErr.Error())
+					break
+				}
+
+				// Append the loaded user to the users slice under a lock.
+				mu.Lock()
+				users = append(users, &serializer.IncidentCaller{
+					MattermostUserID: user.MattermostUserID,
+					Username:         user.Username,
+					ServiceNowUser:   user.ServiceNowUser,
+				})
+				mu.Unlock()
 			}
-		}()
+		}
 
 		if len(kvList) < constants.DefaultPerPage {
 			break
@@ -128,7 +123,6 @@ func (s *pluginStore) GetAllUsers() ([]*serializer.IncidentCaller, error) {
 		page++
 	}
 
-	wg.Wait()
 	return users, nil
 }
 
@@ -139,17 +133,18 @@ func (s *pluginStore) DeleteUserTokenOnEncryptionSecretChange() {
 	}
 
 	deleteAllUsersMutex.Lock()
-	if deleteAllUsersState := s.DeleteAllUsersState(); !deleteAllUsersState {
-		deleteAllUsersMutex.Unlock()
+	deleteAllUsersState := s.DeleteAllUsersState()
+	deleteAllUsersMutex.Unlock()
+
+	if !deleteAllUsersState {
 		return
 	}
 
 	defer func() {
 		if err = s.basicKV.Delete(constants.DeleteAllUsersKey); err != nil {
-			s.plugin.API.LogError("Unable to delete all users state from the store", "Error", err.Error())
+			s.plugin.API.LogError("Unable to disconnect users job running flag from the store", "Error", err.Error())
 		}
 	}()
-	deleteAllUsersMutex.Unlock()
 	users, err := s.GetAllUsers()
 	if err != nil {
 		s.plugin.API.LogError(constants.ErrorGetUsers, "Error", err.Error())
@@ -167,13 +162,13 @@ func (s *pluginStore) DeleteUserTokenOnEncryptionSecretChange() {
 func (s *pluginStore) DeleteAllUsersState() bool {
 	key, err := s.basicKV.Load(constants.DeleteAllUsersKey)
 	if err != nil && err.Error() != "not found" {
-		s.plugin.API.LogError("Unable to get delete all users state from the store", "Error", err.Error())
+		s.plugin.API.LogError("Unable to get disconnect users job running flag from the store", "Error", err.Error())
 		return false
 	}
 
 	if len(key) == 0 {
 		if err := s.basicKV.Store(constants.DeleteAllUsersKey, []byte{1}); err != nil {
-			s.plugin.API.LogError("Unable to store delete all users state from the store", "Error", err.Error())
+			s.plugin.API.LogError("Unable to store disconnect users job running flag from the store", "Error", err.Error())
 			return false
 		}
 
